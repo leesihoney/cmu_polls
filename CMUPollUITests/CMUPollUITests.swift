655ebd3b3d7f37b70e2f831fc1478d4d18d1dbf6
Sungho Cho <sunghoch@andrew.cmu.edu>
2019-10-23 21:17:12 -0400

    Initial Commit

---
+++ CMUPollUITests/CMUPollUITests.swift
@@ -0,0 +1,34 @@
+//
+//  CMUPollUITests.swift
+//  CMUPollUITests
+//
+//  Created by Sung on 10/23/19.
+//  Copyright © 2019 67442. All rights reserved.
+//
+
+import XCTest
+
+class CMUPollUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
