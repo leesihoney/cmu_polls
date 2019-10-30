2b65b3fb245fabe4d025969d6e5f82cda69b95f0
Aiden Lee <aidenlee@Aidens-MBP.wv.cc.cmu.edu>
2019-10-29 22:10:04 -0400

    basic logIn Page

--- CMUPoll/CMUPoll/ContentView.swift
+++ CMUPoll/CMUPoll/ContentView.swift
@@ -9,10 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+  @State private var username: String = ""
+  @State private var password: String = ""
+  
     var body: some View {
-        Text("Hello World")
-    }
-}
+      
+
+      VStack(alignment: .leading, spacing: 10) {
+        
+        HStack(alignment: .center) {
+          Image("Logo").resizable()
+            .frame(width: 360, height: 100)
+        }
+        
+        Text("Username").font(.headline).padding(5)
+        TextField("", text: $username)
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+        
+        
+        
+        Text("Password").font(.headline).padding(5)
+        SecureField("", text: $password)
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+        
+        Button(action: {
+          print("username is \(self.username) \n" +
+            "password is \(self.password)")
+        })
+          {
+          HStack(alignment: .center, spacing: 10) {
+            Spacer()
+            Text("Login").shadow(radius: 10)
+            Spacer()
+            }
+          }
+        
+      }
+      }
+  
+  }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
