adda9022da63144cd9ef8f9f657accad70bbecaa
Sungho Cho <sunghoch@andrew.cmu.edu>
2019-10-31 07:49:18 -0400

    Add necessary files from 'api' branch including pods

--- CMUPoll/AppDelegate.swift
+++ CMUPoll/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    FirebaseApp.configure()
     return true
   }
 
@@ -47,7 +49,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
           if let error = error as NSError? {
               // Replace this implementation with code to handle the error appropriately.
               // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-               
+
               /*
                Typical reasons for an error here include:
                * The parent directory does not exist, cannot be created, or disallows writing.
@@ -79,4 +81,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
 
 }
-
---
+++ Podfile
@@ -0,0 +1,29 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CMUPoll' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CMUPoll
+  # Add the Firebase pod for Google Analytics
+  pod 'Firebase/Analytics'
+
+  # Add the pods for any other Firebase products you want to use in your app
+  # For example, to use Firebase Authentication and Cloud Firestore
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+
+  pod 'GoogleSignIn'
+
+  target 'CMUPollTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CMUPollUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
