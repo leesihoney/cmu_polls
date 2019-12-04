88712de0e0e957b22bc734f64b0ea186a79b46aa
= <siheonl@andrew.cmu.edu>
2019-12-04 12:32:01 -0500

    xcode 11.2 version sucks so I had to literally fake the interaction

--- CMUPoll/Views/AllPollsView.swift
+++ CMUPoll/Views/AllPollsView.swift
@@ -13,14 +13,17 @@ struct AllPollsView: View {
   @State var polls = [Poll]()
   @State var tags = [Tag]()
   @State private var searchTerm: String = ""
+  @State private var showingAddView: Bool = false
+  
   
   var body: some View {
     NavigationView {
       
       VStack(alignment: .leading, spacing: 10) {
+        //HACK (iOS version hack)
+        HiddenNavigationLink(destination: PollCreateView(refresh: self.getUserPolls), isActive: self.$showingAddView)
         
-        SearchBarView(text: $searchTerm)
-          .padding(.vertical, CGFloat(8))
+        SearchBarView(text: $searchTerm)        .padding(.vertical, CGFloat(8))
           .padding(.horizontal, 16)
         
         TagsView(tags: tags)
@@ -33,20 +36,24 @@ struct AllPollsView: View {
           .foregroundColor(Color.gray)
           .padding(.horizontal, 16)
         
-        List(self.polls.filter {
-          self.searchTerm.isEmpty ? true : $0.title.localizedCaseInsensitiveContains(self.searchTerm)
-        }) { poll in
-          NavigationLink(destination: PollDetailView(poll: poll)) {
-            PollView(poll: poll)
+        ScrollView {
+          VStack(alignment: .leading, spacing: 8) {
+            ForEach(self.polls.filter {
+              self.searchTerm.isEmpty ? true : $0.title.localizedCaseInsensitiveContains(self.searchTerm)
+            }) { poll in
+              NavigationLink(destination: PollDetailView(poll: poll)) {
+                PollView(poll: poll)
+              }
+              .buttonStyle(PlainButtonStyle())
+            }
           }
         }
         .navigationBarTitle(Text("CMUPoll"), displayMode: .inline)
         .navigationBarItems(trailing:
-          NavigationLink(destination: PollCreateView(refresh: self.getUserPolls)) {
-            Text("Add")
-          }
+          Button("Add") { self.showingAddView = true }
         )
       }
+      .background(Color(red: 248 / 255.0, green: 248 / 255.0, blue: 248 / 255.0))
     }
     .onAppear {
       self.getUserPolls()
