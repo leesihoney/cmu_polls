221642dedeb0878825f9c0d3f52db8b673964f4b
= <siheonl@andrew.cmu.edu>
2019-12-04 12:33:37 -0500

    fake hidden navigation link for current xcode ver

--- CMUPoll/Views/HiddenNavigationLink.swift
+++ CMUPoll/Views/HiddenNavigationLink.swift
@@ -8,14 +8,31 @@
 
 import SwiftUI
 
-struct HiddenNavigationLink: View {
+struct HiddenNavigationLink<Destination : View>: View {
+    public var destination: Destination
+    public var isActive: Binding<Bool>
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        NavigationLink(destination: self.destination, isActive: self.isActive)
+        { EmptyView() }
+            .frame(width: 0, height: 0)
+            .disabled(true)
+            .hidden()
     }
 }
 
-struct HiddenNavigationLink_Previews: PreviewProvider {
-    static var previews: some View {
-        HiddenNavigationLink()
+struct ActivateButton<Label> : View where Label : View {
+
+    public var activates: Binding<Bool>
+    public var label: Label
+
+    public init(activates: Binding<Bool>, @ViewBuilder label: () -> Label) {
+        self.activates = activates
+        self.label = label()
+    }
+
+    var body: some View {
+        Button(action: { self.activates.wrappedValue = true }, label: { self.label } )
     }
 }
