276171e30d97d9f3355e6ef357db7b24dee55ee4
Sungho Cho <sunghoch@andrew.cmu.edu>
2019-12-05 01:48:40 -0500

    Add private checkbox

--- CMUPoll/Views/PollCreateView.swift
+++ CMUPoll/Views/PollCreateView.swift
@@ -42,10 +42,9 @@ struct PollCreateView: View {
   @State var description = ""
   @State var quantity: Int = 1
   @State var questions: [QuestionInput] = [QuestionInput()]
+  @State var is_private = false
   
   @Environment(\.presentationMode) var presentation
-  
-  
   
   var body: some View {
     KeyboardHost {
@@ -54,8 +53,35 @@ struct PollCreateView: View {
         if (user != nil) {
           UploaderProfileView(uploaderName: "\(user!.first_name) \(user!.last_name)", uploaderMajor: user!.major, uploaderGraduationYear: String(user!.graduation_year ?? 2020))
         }
+        VStack(alignment: .leading, spacing: 10) {
+          HStack {
+            Text(verbatim: "Private")
+              .font(Font.system(size: 16, design: .default))
+              .foregroundColor(Color.gray)
+              .onTapGesture {
+                self.is_private = !self.is_private
+            }
+            Spacer()
+            if (is_private == true) {
+              Image(systemName: "checkmark.square")
+                .foregroundColor(.gray)
+                .font(.system(size: 24))
+                .onTapGesture {
+                  self.is_private = false
+              }
+            } else {
+              Image(systemName: "square")
+                .foregroundColor(.gray)
+                .font(.system(size: 24))
+                .onTapGesture {
+                  self.is_private = true
+              }
+            }
+          }
+        }
         
         VStack(alignment: .leading, spacing: 13) {
+          
           TextField("Enter title", text: $title)
             .font(Font.system(size: 30, design: .default))
             .textFieldStyle(PlainTextFieldStyle())
@@ -66,7 +92,7 @@ struct PollCreateView: View {
             .padding(EdgeInsets(top: 0, leading: 0, bottom: 3, trailing: 10))
         }
         
-        VStack(alignment: .leading, spacing: 13) {
+        VStack(alignment: .leading, spacing: 8) {
           Text(verbatim: "Tags (comma-separated)")
             .font(Font.system(size: 15, design: .default))
             .foregroundColor(Color.gray)
@@ -81,7 +107,7 @@ struct PollCreateView: View {
             .padding(EdgeInsets(top: 0, leading: 0, bottom: 3, trailing: 10))
         }
         
-        VStack(alignment: .leading, spacing: 13) {
+        VStack(alignment: .leading, spacing: 8) {
           Text(verbatim: "Description")
             .font(Font.system(size: 15, design: .default))
             .foregroundColor(Color.gray)
@@ -89,7 +115,7 @@ struct PollCreateView: View {
             .frame(width: nil, height: 20)
             .font(Font.system(size: 15, design: .default))
             .textFieldStyle(PlainTextFieldStyle())
-            .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 10))
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
           Rectangle()
             .fill(Color.gray)
             .frame(width: nil, height: 0.5)
@@ -144,7 +170,7 @@ struct PollCreateView: View {
         ), trailing:
         // TODO: should connect to a form view
         Button("Save") {
-          Poll.create(title: self.title, description: self.description, link: "", is_private: false, is_closed: false, passcode: "NULL", completion: { poll in
+          Poll.create(title: self.title, description: self.description, link: "", is_private: self.is_private, is_closed: false, passcode: "NULL", completion: { poll in
             print("the poll has been created!")
             for questionInput in self.questions {
               Question.create(is_multiple_choice: false, title: questionInput.title, poll_id: poll.id, completion: { question in
