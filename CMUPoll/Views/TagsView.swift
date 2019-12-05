bf9944e5039404dcd3a88bd0eb2997d44b85e2e9
Aiden Lee <aidenlee@Chriss-MBP-3.wv.cc.cmu.edu>
2019-12-04 21:46:21 -0500

    tag is now clickable

--- CMUPoll/Views/TagsView.swift
+++ CMUPoll/Views/TagsView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 
 struct TagsView: View {
   let tags: [Tag]
+  @State var uponTagTap: ([Poll]) -> ()
+  
   var body: some View {
     VStack(alignment: .leading, spacing: 7) {
       Text(verbatim: "POPULAR KEYWORDS")
@@ -19,23 +21,25 @@ struct TagsView: View {
       ScrollView(.horizontal, content: {
         HStack(spacing: CGFloat(7.0)) {
           ForEach(tags) { tag in
-            TagFilterView(tagText: tag.name)
+            TagFilterView(tagText: tag.name, uponTagTap: { tag_polls in
+              // Function body
+              self.uponTagTap(tag_polls)}
+            )
           }
         }
       })
     }
-    
   }
 }
 
-struct TagsView_Previews: PreviewProvider {
-  static var previews: some View {
-    TagsView(tags: [
-      Tag(id: "1", name: "IS"),
-      Tag(id: "3", name: "CS"),
-      Tag(id: "4", name: "Life"),
-      Tag(id: "5", name: "Academic"),
-      Tag(id: "8", name: "Food"),
-    ])
-  }
-}
+//struct TagsView_Previews: PreviewProvider {
+//  static var previews: some View {
+//    TagsView(tags: [
+//      Tag(id: "1", name: "IS"),
+//      Tag(id: "3", name: "CS"),
+//      Tag(id: "4", name: "Life"),
+//      Tag(id: "5", name: "Academic"),
+//      Tag(id: "8", name: "Food"),
+//    ])
+//  }
+//}
