82f11d445740ea1aa4ff925e20e5cc3c870bc45f
= <siheonl@andrew.cmu.edu>
2019-12-05 02:19:55 -0500

    PollDetailView has been updated with new comments section

--- CMUPoll/Views/PollDetailView.swift
+++ CMUPoll/Views/PollDetailView.swift
@@ -7,13 +7,9 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct PollDetailView: View {
-  let profile = Image("user_pic")
-  let uploaderName = "Aiden Lee"
-  let uploaderMajor = "Information Systems"
-  let uploaderGraduationYear: String = "2020"
-  let uploadedDaysAgo = "29"
   let poll: Poll
   @State var tags = [Tag]()
   @State var questions = [Question]()
@@ -25,110 +21,209 @@ struct PollDetailView: View {
   @State var initialized: Bool = false
   @Environment(\.presentationMode) var presentation
   @State var userAlreadyLiked: Bool = false
+  @State var replyingComment: Comment? = nil
+  @State var replyingCommentUser: User?
+  @State var commentContent: String = ""
+  @State var commentsLoaded = false
+
   
   
   var body: some View {
-    ScrollView() {
-      VStack(alignment: .leading) {
-        if (pollUser != nil) {
-          PollUploaderProfileView(uploaderName: "\(pollUser!.first_name) \(pollUser!.last_name)", uploaderMajor: pollUser!.major, uploaderGraduationYear: String(pollUser!.graduation_year ?? 2020), uploadedDaysAgo: uploadedDaysAgo)
-        }
-        
-        HStack(alignment: .firstTextBaseline) {
-          Text(poll.title)
+    VStack(alignment: .leading) {
+      ScrollView() {
+        VStack(alignment: .leading) {
+          if (pollUser != nil) {
+            PollUploaderProfileView(uploaderName: "\(pollUser!.first_name) \(pollUser!.last_name)", uploaderMajor: pollUser!.major, uploaderGraduationYear: String(pollUser!.graduation_year ?? 2020), uploadedDaysAgo: self.poll.getDateDisplayString())
+          }
+          
+          HStack(alignment: .firstTextBaseline) {
+            Text(poll.title)
+              .fontWeight(.semibold)
+              .multilineTextAlignment(.leading)
+              .font(Font.system(size: 20, design: .default))
+              .lineSpacing(10)
+            
+            Spacer()
+            
+            if !self.userAlreadyLiked {
+              Button(action: {
+                print("like button is clicked!")
+                print("here, likes: \(self.likes)")
+                self.addLike()
+                self.getPollLikes()
+              }) {
+                Image(systemName: "hand.thumbsup")
+                  .foregroundColor(.gray)
+                  .frame(width: CGFloat(20.0), height: CGFloat(20.0)
+                    ,alignment: .bottomLeading)
+                Text("Like")
+                  .fontWeight(.regular)
+                  .foregroundColor(Color.gray)
+                  .font(Font.system(size: 10, design: .default))
+              }
+            }
+            else {
+              Button(action: {
+                print("like button is unclicked!")
+                print("here, likes: \(self.likes)")
+                self.deleteLike()
+                self.getPollLikes()
+              }) {
+                Image(systemName: "hand.thumbsup.fill")
+                  .foregroundColor(.gray)
+                  .frame(width: CGFloat(20.0), height: CGFloat(20.0)
+                    ,alignment: .bottomLeading)
+                Text("Unlike")
+                  .fontWeight(.regular)
+                  .foregroundColor(Color.gray)
+                  .font(Font.system(size: 10, design: .default))
+              }
+            }
+          }
+          
+          HStack(alignment: .firstTextBaseline, spacing: 5) {
+            ForEach(self.tags) { tag in
+              TagView(tagText: tag.name)
+            }
+          }
+          .padding(.vertical, 14)
+          
+          PollDetailDescriptionView(description: poll.description)
+          Text(verbatim: "You will get 5 point per questions that you answered")
+            .font(Font.system(size: 12, design: .default))
             .fontWeight(.semibold)
-            .multilineTextAlignment(.leading)
-            .font(Font.system(size: 20, design: .default))
-            .lineSpacing(10)
+            .foregroundColor(Color(red: 236 / 255.0, green: 0 / 255.0, blue: 0 / 255.0))
+            .padding(.vertical, 24)
           
-          Spacer()
-          
-          if !self.userAlreadyLiked {
-            Button(action: {
-              print("like button is clicked!")
-              print("here, likes: \(self.likes)")
-              self.addLike()
-              self.getPollLikes()
-            }) {
-              Image(systemName: "hand.thumbsup")
-                .foregroundColor(.gray)
-                .frame(width: CGFloat(20.0), height: CGFloat(20.0)
-                  ,alignment: .bottomLeading)
-              Text("Like")
-                .fontWeight(.regular)
-                .foregroundColor(Color.gray)
-                .font(Font.system(size: 10, design: .default))
+          if self.initialized {
+            ForEach(self.questions) { question in
+              if self.questionAnswered[question.id] == true {
+                AnswerGraphView(question: question, onEditedAnswer: {
+                  self.questionAnswered[question.id] = false
+                })
+              } else {
+                AnswerBoxView(question: question, onNewAnswer: {
+                  self.questionAnswered[question.id] = true
+                })
+              }
             }
           }
-          else {
-            Button(action: {
-              print("like button is unclicked!")
-              print("here, likes: \(self.likes)")
-              self.deleteLike()
-              self.getPollLikes()
-            }) {
-              Image(systemName: "hand.thumbsup.fill")
-                .foregroundColor(.gray)
-                .frame(width: CGFloat(20.0), height: CGFloat(20.0)
-                  ,alignment: .bottomLeading)
-              Text("Unlike")
-                .fontWeight(.regular)
-                .foregroundColor(Color.gray)
-                .font(Font.system(size: 10, design: .default))
-            }
-          }
-        }
-        
-        HStack(alignment: .firstTextBaseline, spacing: 5) {
-          ForEach(self.tags) { tag in
-            TagView(tagText: tag.name)
-            }
-        }
-        .padding(.vertical, 14)
-        
-        PollDetailDescriptionView(description: poll.description)
-        Text(verbatim: "You will get 5 point per questions that you answered")
-          .font(Font.system(size: 12, design: .default))
-          .fontWeight(.semibold)
-          .foregroundColor(Color(red: 236 / 255.0, green: 0 / 255.0, blue: 0 / 255.0))
-          .padding(.vertical, 24)
-        
-        if self.initialized {
-          ForEach(self.questions) { question in
-            if self.questionAnswered[question.id] == true {
-              AnswerGraphView(question: question, onEditedAnswer: {
-                self.questionAnswered[question.id] = false
-              })
+          
+          VStack(alignment: .leading, spacing: 10) {
+            Text("Comments \(self.comments.count)")
+              .font(Font.system(size: 18, design: .default))
+              .fontWeight(.semibold)
+              .foregroundColor(Color(red: 91 / 255.0, green: 91 / 255.0, blue: 91 / 255.0))
+            
+            if (self.commentsLoaded && self.comments.count > 0) {
+              VStack(alignment: .leading, spacing: 9) {
+                ForEach(self.comments) { comment in
+                  CommentView(comment: comment, onReply: {
+                    self.replyingComment = comment
+                    self.replyingComment!.user(completion: { user in
+                      DispatchQueue.main.async {
+                        self.replyingCommentUser = user
+                      }
+                    })
+                  })
+                }
+              }
+              .padding(.leading, 7)
             } else {
-              AnswerBoxView(question: question, onNewAnswer: {
-                self.questionAnswered[question.id] = true
-              })
+              HStack(alignment: .top) {
+                Spacer()
+                Text("No comments posted for this poll!")
+                  .font(Font.system(size: 12, design: .default))
+                  .fontWeight(.medium)
+                  .foregroundColor(Color(red: 91 / 255.0, green: 91 / 255.0, blue: 91 / 255.0))
+                Spacer()
+              }
+              .padding(.vertical, 24)
             }
+            
           }
+          .padding(.top, 33)
         }
-        
-        PollDetailCommentsView(comments: self.comments)
-        .padding(.top, 33)
-
-      }
-      .navigationBarItems(leading: Button (
-        action: {
-          DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+        .navigationBarItems(leading: Button (
+          action: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
               self.presentation.wrappedValue.dismiss()
+            }
+        }, label: { Text("Back") }
+        ))
+      }
+      .frame(minWidth: 0, maxWidth: .infinity, idealHeight: 188.0, alignment: .topLeading)
+      .multilineTextAlignment(.leading)
+      .padding(.vertical, 25)
+      .padding(.horizontal, 15)
+      .onAppear {
+        self.initialized = false
+        self.getPollUser()
+        self.getPollComments()
+        self.getPollLikes()
+        self.resetCommentsLoaded()
+      }
+      VStack {
+        if self.replyingComment != nil && self.replyingCommentUser != nil {
+          HStack(alignment: .top, spacing: 20) {
+            VStack(alignment: .leading, spacing: 4) {
+              Text("\(self.replyingCommentUser!.first_name) \(self.replyingCommentUser!.last_name)")
+                .font(Font.system(size: 12, design: .default))
+                .fontWeight(.bold)
+                .foregroundColor(Color(red: 91 / 255.0, green: 91 / 255.0, blue: 91 / 255.0))
+              Text("\(self.replyingComment!.content)")
+                .font(Font.system(size: 12, design: .default))
+                .fontWeight(.regular)
+                .foregroundColor(Color(red: 91 / 255.0, green: 91 / 255.0, blue: 91 / 255.0))
+                .lineLimit(1)
+
+            }
+            Spacer()
+            Button (
+              action: {
+                self.replyingCommentUser = nil
+                self.replyingComment = nil
+            }, label: {
+              Image(systemName: "xmark.circle.fill")
+              .font(.system(size: 24))
+
+              
+            }
+            )
           }
-      }, label: { Text("Back") }
-      ))
+        .padding()
+          
+        }
+        HStack {
+          TextField("Enter Comments", text: $commentContent)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding(.trailing, 4)
+          Button(
+            action: {
+              if self.commentContent.count > 0 {
+                Comment.create(content: self.commentContent, comment_id: self.replyingComment?.id, poll_id: self.poll.id ,completion: { comment in
+                  print("the comment has been created!")
+                  self.commentContent = ""
+                  self.replyingCommentUser = nil
+                  self.replyingComment = nil
+                  self.getPollComments()
+                  self.resetCommentsLoaded()
+                })
+              }
+              
+          }, label: { Text("Post") }
+          )
+        }
+        .padding()
+      }
+      .background(Color(red: 248 / 255.0, green: 248 / 255.0, blue: 248 / 255.0, opacity: 0.92))
     }
-    .frame(minWidth: 0, maxWidth: .infinity, idealHeight: 188.0, alignment: .topLeading)
-    .multilineTextAlignment(.leading)
-    .padding(.vertical, 25)
-    .padding(.horizontal, 15)
-    .onAppear {
-      self.initialized = false
-      self.getPollUser()
-      self.getPollComments()
-      self.getPollLikes()
-    }
+    
+  }
+  
+  func resetCommentsLoaded() {
+    self.commentsLoaded = false
+    self.commentsLoaded = true
   }
 
   
@@ -217,18 +312,6 @@ struct PollDetailView: View {
   }
 }
 
-struct PollDetailCommentsView: View {
-  let comments: [Comment]
-  
-  var body: some View {
-    VStack(alignment: .leading) {
-      Text("Comments \(comments.count)")
-      .font(Font.system(size: 18, design: .default))
-      .fontWeight(.semibold)
-      .foregroundColor(Color(red: 91 / 255.0, green: 91 / 255.0, blue: 91 / 255.0))
-    }
-  }
-}
 struct PollDetailView_Previews: PreviewProvider {
   static var previews: some View {
     PollDetailView(poll: Poll(id: "1", user_id: "1", title: "Who is your favorite IS Professor?", description: "Nyo", posted_at: "2019-10-24", link: "", is_private: false, is_closed: false))
